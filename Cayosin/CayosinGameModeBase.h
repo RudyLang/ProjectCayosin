0b37d9670035dea1a87643b5345f79aebdb9c32b
RudyLang <r.lang.dev@gmail.com>
2020-09-07 15:16:59 -0400

    Add initial Unreal source content.

---
+++ Cayosin/CayosinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CayosinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAYOSIN_API ACayosinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
